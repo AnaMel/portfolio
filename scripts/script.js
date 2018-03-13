adeed0b57b4a85c6c13bc5f765dba8db96966d2f
Anastasiya Melentyeva <anastasya.melenteva@gmail.com>
2018-03-12 23:18:35 -0400

    Finish css

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,28 @@ $(function(){
         delay: 100,
     });
 });
+
+$("label.toggle").click(function () {
+    $('ul.target').toggleClass('visibility');
+    $('label.toggle-back').toggleClass('hide');
+    $('label.toggle').toggleClass('hide');
+});
+$("label.toggle-back").click(function () {
+    $('ul.target').toggleClass('visibility');
+    $('label.toggle-back').toggleClass('hide');
+    $('label.toggle').toggleClass('hide');
+});
+// Ensure that class .hide is removed from menu icon if the screen is resized into desktop view when the menu is open
+var toggle = $('label.toggle');
+var toggleBack = $('label.toggle-back');    
+var ul = $('ul.target');
+
+//get the screen width
+$(window).resize(function(){
+    var screenWidth = $(this).width();
+    if(screenWidth > 768 ) {
+        toggleBack.addClass('hide');
+        toggle.removeClass('hide');
+        ul.removeClass('visibility');
+    }
+});
